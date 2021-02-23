d8c7f28a52678e80f0e2b6088d1b884b4a56b672
Himanshu <arorahimanshu472@gmail.com>
2021-02-23 18:08:54 +0530

    stack using array

---
+++ stack/StackUsingArray.cpp
@@ -0,0 +1,54 @@
+
+template<typename T>
+class Stack {
+	T *arr;
+	int capacity;
+	int nextIndex;
+	
+	public:
+		
+		Stack() {
+			capacity = 10;
+			arr = new T[10];
+			nextIndex = 0; 
+		}
+		
+		int getSize() {
+			return nextIndex;
+		}
+		
+		// insert Element
+		void push(int data) {
+			if(size == capacity) {
+				T *newArr = new T[capacity * 2];
+				for(int i = 0; i < capacity; i++)
+					newArr[i] = arr[i];
+					capacity *= 2;
+				delete [] arr;
+				arr = newArr;
+			}
+			arr[nextIndex] = data;
+			++nextIndex;
+		}
+		
+		//  delete Element
+		void pop() {
+			if(size == 0) {
+				cout << "Stack is empty" << endl;
+				return;
+			}
+			--nextIndex;
+			--size;
+		}
+		
+		// element on top
+		T top() {
+			if(size == 0) {
+				cout << "Stack is empty" << endl;
+				return -1;
+			}
+			return arr[nextIndex-1];
+		}
+		
+};
+
