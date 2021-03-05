bb799cb23fe9faf8a3294c3a767dee9c6dff964e
Himanshu <arorahimanshu472@gmail.com>
2021-03-05 16:49:42 +0530

    Priority Queue Added

---
+++ priorityQueue/implementation.cpp
@@ -0,0 +1,73 @@
+
+
+class PriorityQueue {
+	vector<int> pq;
+	
+	public:
+		
+		int getSize() {
+			return pq.size();
+		}
+		
+		bool isEmpty() {
+			return pq.size() == 0;
+ 		}
+ 		
+ 		int getMin() {
+ 			if(isEmpty())
+			 return 0;
+			return pq[0];	
+		}
+		 
+		void insert(int data) {
+			pq.push_back(data);
+			int childIndex = pq.size() - 1;
+			int parentIndex = (childIndex - 1) / 2;
+			while(pq[parentIndex] > pq[childIndex]) {
+				// swaping elements
+				int temp = pq[parentIndex];
+				pq[parentIndex] = pq[childIndex];
+				pq[childIndex] = temp;
+				
+				childIndex = parentIndex;
+				if(childIndex == 0) break;
+				parentIndex = (childIndex - 1) / 2;
+			}
+		}
+		
+		void remove(){
+			if(isEmpty()) {
+				cout << "Queue is Empty" << endl;
+				return;
+			}
+			pq[0] = pq[pq.size() - 1];
+ 			pq.pop_back();
+			int parentIndex = 0;
+			int leftChildIndex = 2*parentIndex + 1;
+			int rightChildIndex = 2*parentIndex + 2;
+			while(leftChildIndex < pq.size()) {
+				int minIndex = parentIndex;
+				if(pq[minIndex] > pq[leftChildIndex]) //   Note ----- we are using minIndex not parentIndex
+					minIndex = leftChildIndex; 
+				if(rightChildIndex < pq.size() && pq[minIndex] > pq[rightChildIndex]) 
+					minIndex = rightChildIndex;
+					
+				// element is at correct position
+				if(minIndex == parentIndex)
+					break;
+				
+				// swaping elements
+				int temp = pq[parentIndex];
+				pq[parentIndex] = pq[minIndex];
+				pq[minIndex] = temp;
+				
+				parentIndex = minIndex;
+				leftChildIndex = 2*parentIndex + 1;
+				rightChildIndex = 2*parentIndex + 2;
+			
+			}
+			
+		}
+};
+
+
